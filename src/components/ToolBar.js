4b96da68213a439d194b582fd145218352da228a
Ville Jänkälä <ville.jankala@gmail.com>
2017-12-19 22:05:19 +0200

    Added correct github repository url

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -6,7 +6,7 @@ class ToolBar extends Component {
     return (
       <div className="ToolBar">
         <a href="" title="Pick channels" onClick={(e) => this.openChannelPicker(e)}><i className="fas fa-list-alt fa-2x"></i></a>
-        <a href="" title="Fork me on GitHub" target="_blank"><i className="fab fa-github fa-2x"></i></a>
+        <a href="https://github.com/ville-j/tvopas" title="Fork me on GitHub" target="_blank"><i className="fab fa-github fa-2x"></i></a>
       </div>
     )
   }
